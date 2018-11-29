ebd9bb7e9bfbe7b15d72bb36a20f85017a813b17
Josh Grazda <joshuagrazda@yahoo.com>
2018-11-29 16:47:27 -0500

    Level 2 Change and Implemented Pressure Plates with Movable Objects Added: - Pressure plates and movable objects Changed: - Level 2 changed to have new mechanic

---
+++ MontrealAdvSem/Assets/Scripts/Objective/MovableScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovableScript : MonoBehaviour 
+{
+	[Header("Move variables")]
+	[SerializeField]
+	private float endX;
+	[SerializeField]
+	public float endY;
+	private Vector3 startPostion;
+	private Vector3 endPosition;
+	[SerializeField]
+	public float moveDuration;
+
+	[Header("Bool variables")]
+	public bool move;
+	public bool resetMove;
+
+	void Start () 
+	{
+		//Set positions
+		startPostion = gameObject.transform.position;
+		endPosition = new Vector3(endX, endY, gameObject.transform.position.z);
+	}
+	
+	void Update () 
+	{
+		CheckMove();
+	}
+
+	private void CheckMove()
+	{
+		if(move)
+		{
+			MoveObject();
+			move = false;
+		}
+		if(resetMove)
+		{
+			ResetObject();
+			resetMove = false;
+		}
+	}
+
+	private void MoveObject()
+	{
+		transform.DOMove(endPosition, moveDuration, false);
+	}
+
+	private void ResetObject()
+	{
+		transform.DOMove(startPostion, moveDuration, false);
+	}
+}
---
+++ MontrealAdvSem/Assets/Scripts/Objective/PlateScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject movableObject;
+
+    private List<Collider2D> colliderList;
+
+    void Start()
+    {
+        colliderList = new List<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player" || col.tag == "Dormant")
+        {
+			//Add object that entered trigger
+            colliderList.Add(col);
+            movableObject.GetComponent<MovableScript>().move = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player" || col.tag == "Dormant")
+        {
+			//Remove object that left collider
+            if (colliderList.Contains(col))
+            {
+                colliderList.Remove(col);
+            }
+			//If Empty reset move
+            if (colliderList.Count == 0)
+            {
+                movableObject.GetComponent<MovableScript>().resetMove = true;
+            }
+        }
+    }
+}
